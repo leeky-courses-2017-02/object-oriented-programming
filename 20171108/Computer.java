5de3b57653cd2cacbc39c68bfe094827ab554911
Kyungyong Lee <leeky@kookmin.ac.kr>
2017-11-09 10:01:32 +0900

    update

--- 20171108/Computer.java
+++
@@ -1,5 +0,0 @@
-package kr.ac.kookmin.cs.oop.homework.four;
-
-public class Computer {
-
-}
--- 20171108/ScienceLibrary.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.kookmin.cs.oop.homework.four;
-
-public class ScienceLibrary extends Library {
-    public ScienceLibrary() {
-
-    }
-
-    private Computer[] computers;
-}
